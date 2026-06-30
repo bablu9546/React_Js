7706fdf3c2876b3c39ac4448e1fe6987ae0c02f0
Mr.B_P1806 <120113164+bablu9546@users.noreply.github.com>
2026-07-01 00:32:43 +0530

    lazy_loading
    Lazy loading is a design pattern that delays the loading of non-critical resources until they are actually neede.

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,20 @@
+import React, { lazy, Suspense, useState } from 'react';
+//import User from './User';
 
-//  import Home from './Home.jsx';
-//  import About from './About.jsx';
+const User=lazy(()=>import('./User')); 
 
-// import { Route, Routes, Link } from "react-router";
+function App(){
+  const[load,setLoad]=useState(false);
+  return(
+    <div>
+      <h1>LazY Loading</h1>
+      {
+        load? <Suspense fallback={<h3>loading...</h3>}><User/></Suspense>:null
+      }
+      
 
-// function App(){
-//   return(
-//     <>
-
-//     <Link to="/">Home </Link><br />
-//     <Link to="/about">About </Link>
-
-//     <Routes>
-//       <Route path="/" element={<Home/>} />
-//       <Route path="/about" element={<About/>} />
-//     </Routes>
-//     </>
-//   )
-// }
-// export default App;
-
-
-
-
-
-
-
-
-//uses Router 
-
-import { Link, Route, Routes } from 'react-router';
-import Home from './Home';
-import About from './About';
-import College from './College';
-import Login from './Login';
-import NavBar from './NavBar';
-import PageNotFound from './PageNotFound';
-import Student from './Student';
-import Department from './Department';
-import Details from './Details';
-import User from './User';
-import UserDetails from './UserDetails';
-import UserList from './UserList';
-
-
-function App() {
-  return (
-    <>
-      {/* <NavBar/> */}
-
-      <Routes>
-
-        <Route element={<NavBar />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/User" element={<User />} />
-          <Route path='/User/List?' element={<UserList />} />
-
-          {/* <Route path="/User/:id" element={<UserDetails/>} /> */}
-          <Route path="/User/:id/:name?" element={<UserDetails />} />
-
-          <Route path='in'>
-            <Route path='/in/user'>
-              <Route path="/in/user/Login" element={<Login />} />
-              <Route path="/in/user/About" element={<About />} />
-            </Route>
-          </Route>
-
-        </Route>
-
-
-
-
-        <Route path="/College" element={<College />}>
-          <Route index element={<Student />} />
-          <Route path="Department" element={<Department />} />
-          <Route path="Details" element={<Details />} />
-        </Route>
-
-        <Route path='/*' element={<PageNotFound />} />
-
-        {/* <Route path='/*' element={<Home/>}/> */}
-        {/* <Route path='/*' element={<Login />} /> */}
-
-
-        {/* <Route path="/*" element={<Navigate to="/Login" />} />   any error this link  */}
-
-      </Routes>
-    </>
+      <button onClick={(event)=>setLoad(true)}>Load User</button>
+    </div>
   )
 }
 export default App;
